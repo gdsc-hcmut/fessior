3263ff7292544ad64b2e930a410532979063b928
lygioian <lygioian@gmail.com>
2023-07-20 20:52:16 +0700

    feat: setting security middleware

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,47 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { Logger as NestLogger } from '@nestjs/common';
 import { ValidationPipe, VersioningType } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 import { Logger, LoggerErrorInterceptor } from 'nestjs-pino';
+import type { NestExpressApplication } from '@nestjs/platform-express';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { bufferLogs: true });
-  const configService = app.get(ConfigService);
+import { middleware } from './app.middleware';
+import { AppModule } from './app.module';
+
+async function bootstrap(): Promise<string> {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    bufferLogs: true,
+  });
+  const isProduction = process.env.NODE_ENV === 'production';
 
   app.useLogger(app.get(Logger));
   app.useGlobalInterceptors(new LoggerErrorInterceptor());
+
   app.useGlobalPipes(new ValidationPipe());
+
+  // Express Middleware
+  middleware(app);
+
+  if (isProduction) {
+    app.enable('trust proxy');
+  }
 
   //Enable versioning v1,v2,...
   app.enableVersioning({
     type: VersioningType.URI,
   });
 
-  const port = configService.get<number>('port') ?? 3000;
-  await app.listen(port);
+  app.enableShutdownHooks();
+
+  await app.listen(process.env.PORT || 3000);
+
+  return app.getUrl();
 }
-bootstrap();
+
+(async (): Promise<void> => {
+  try {
+    const url = await bootstrap();
+    NestLogger.log(url, 'Bootstrap');
+  } catch (error) {
+    NestLogger.error(error, 'Bootstrap');
+  }
+})();
