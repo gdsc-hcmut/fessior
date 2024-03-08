5d75ab44e4a2d7a49f28599fcc1097c2c46771fa
Viet-Duc Nguyen <duc.nv291@gmail.com>
2024-03-08 14:14:02 +0700

    feat: include category list in each url

--- src/urls/urls.module.ts
+++ src/urls/urls.module.ts
@@ -5,9 +5,14 @@ import { Url, UrlSchema } from './schemas/url.schema';
 import { UrlsController } from './urls.controller';
 import { UrlsService } from './urls.service';
 import { AdminOrganizationsModule } from '../admin/organizations/organizations.module';
+import { CategoriesModule } from '../categories/categories.module';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: Url.name, schema: UrlSchema }]), AdminOrganizationsModule],
+  imports: [
+    MongooseModule.forFeature([{ name: Url.name, schema: UrlSchema }]),
+    AdminOrganizationsModule,
+    CategoriesModule,
+  ],
   controllers: [UrlsController],
   providers: [UrlsService],
   exports: [UrlsService],
